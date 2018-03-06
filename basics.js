72ddba7a5544ebc703f8374496a194f562f6606c
Reese Akamine <Arresayy@gmail.com>
2018-03-05 19:34:54 -1000

    complete up to greet function

--- basics.js
+++ basics.js
@@ -1,18 +1,36 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = "Reese";
+console.log(myName);
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+ var person = {
+ 	name: "Reese",
+ 	age: 25,
+ };
+console.log(person);
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive = person.age >= 16;
+ console.log(canDrive);
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+
+function greet(name){
+	return "Hello, my name is " + name;
+}
+console.log(greet('Reese'));
+
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
